43963815fb30815989d92d01486beefe0c089aa0
Saqib Masood <masoodsaqib00@gmail>
2024-02-14 15:01:36 +0530

    knex db connectivity

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+    development: {
+      client: 'pg',
+      connection: {
+        host: 'localhost',
+        port: 5432,
+        user: 'postgres',
+        password: 'Delhi@2001',
+        database: 'todo'
+      }
+    }
+  };
+  
---
+++ migrations/20240213122454_MyMigration.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema.createTable('todos', function(table) {
+      table.increments('id').primary();
+      table.string('task').notNullable();
+      table.boolean('completed').defaultTo(false);
+      table.timestamps(true, true);
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('todos');
+  };
+  
